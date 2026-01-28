56bb756251db4e1ac35af2565348abee3801468a
StuartGIS <62813047+StuartGIS@users.noreply.github.com>
2026-01-28 05:11:45 +0000

    upload draft results csv into code, update tourney info

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,8 @@ elif name_selection == 'Stuart':
 st.divider()
 
 # show tournament info
-st.header("American Express")
-st.markdown("La Quinta Country Club, PGA West, Nicklaus Course  \nLa Quinta, California  \nJanuary 22-25, 2026")
+st.header("Farmers Insurance Open")
+st.markdown("Torrey Pines, North Course, South Course  \nLa Jolla, California  \nJanuary 29- February 1, 2026")
 # Display an image from a local file
 # st.image("/workspaces/degen-projections/el_cardonal.jpeg")
 st.divider()
@@ -262,7 +262,7 @@ dg_pga_live_predictions_df = dg_pga_live_predictions_df.sort_values('current_sco
 styled_dg_live = dg_pga_live_predictions_df.style.apply(style_live_rows, axis=1)
 
 # Join draft results with live predictions and calculate projected points
-draft_results = pd.read_csv("america_express_drafts_results_csv.csv")
+draft_results = pd.read_csv("farmers_2026_draft_results.csv")
 
 merged_players_live_preds_df = pd.merge(draft_results, dg_pga_live_predictions_df, left_on='Player', right_on='player_first_last', how='left')
 # merged_players_live_preds_df['projected_points'] = (
@@ -404,7 +404,7 @@ st.dataframe(styled_dg_live, width='stretch', hide_index=True, column_config={
 st.divider()
 
 # adjust path if needed
-draft_results = pd.read_csv("america_express_drafts_results_csv.csv")
+draft_results = pd.read_csv("farmers_2026_draft_results.csv")
 
 # Anchor for: Draft Results
 st.markdown('<a id="draft-results"></a>', unsafe_allow_html=True)
