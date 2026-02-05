105587623cfb9e29e5a2c3fe5d6194f5b10f0606
StuartGIS <62813047+StuartGIS@users.noreply.github.com>
2026-02-05 05:12:02 +0000

    fix csv name in code

--- streamlit_app.py
+++ streamlit_app.py
@@ -269,7 +269,7 @@ dg_pga_live_predictions_df = dg_pga_live_predictions_df.sort_values('current_sco
 styled_dg_live = dg_pga_live_predictions_df.style.apply(style_live_rows, axis=1)
 
 # Join draft results with live predictions and calculate projected points
-draft_results = pd.read_csv("WM_draft_results.csv")
+draft_results = pd.read_csv("WM_drafts_results.csv")
 
 merged_players_live_preds_df = pd.merge(draft_results, dg_pga_live_predictions_df, left_on='Player', right_on='player_first_last', how='left')
 # merged_players_live_preds_df['projected_points'] = (
@@ -419,7 +419,7 @@ st.dataframe(styled_dg_live, width='stretch', hide_index=True, column_config={
 st.divider()
 
 # adjust path if needed
-draft_results = pd.read_csv("WM_draft_results.csv")
+draft_results = pd.read_csv("WM_drafts_results.csv")
 
 # Anchor for: Draft Results
 st.markdown('<a id="draft-results"></a>', unsafe_allow_html=True)
